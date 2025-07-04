197588906332f5a675146ff1c0d8248dc1223b56
tangphonggmailcom <35770412+tangphonggmailcom@users.noreply.github.com>
2025-07-04 16:59:59 +0700

    Add files via upload

---
+++ flipper3js/externalga4.js
@@ -0,0 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', '', {
+  'fl_book_id': '',
+  'fl_version': ''
+});
